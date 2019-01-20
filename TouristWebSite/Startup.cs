cb7f09a38e39413819cce7b2dad1068c5281d27b
anastasiyapolianska <anastasiyapolianska@gmail.com>
2019-01-20 14:16:25 +0200

    initial

---
+++ TouristWebSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TouristWebSite.Startup))]
+namespace TouristWebSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
