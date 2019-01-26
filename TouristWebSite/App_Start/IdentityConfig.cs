1f157a36b81f49df1dd07b5b823040c0af8492ed
anastasiyapoli <anastasiyapolianska@gmail.com>
2019-01-26 15:24:02 +0200

    Moved database context to separate class.

--- TouristWebSite/App_Start/IdentityConfig.cs
+++ TouristWebSite/App_Start/IdentityConfig.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using System.Web;
+﻿using DAL;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using TouristWebSite.Models;
 
 namespace TouristWebSite
